43427fa06528f4ed72d61ed8bfbefb47f5d595ca
Henri Nieminen <henri@purkki.fi>
2017-08-22 20:07:33 +0300

    Add web app manifest

--- build.sh
+++ build.sh
@@ -18,6 +18,7 @@ if [ -r apikeys ]; then
 			sed -i -e "s/HEREAPPID/${keys[0]}/g" dist/app.min.js
 			sed -i -e "s/HEREAPPCODE/${keys[1]}/g" dist/app.min.js
 			cp src/index.html dist/index.html
+			cp src/manifest.json dist/manifest.json
 			cp src/gfx/* dist/gfx/
 			if [ $? -eq 0 ]; then
 				if [ $# -eq 1 ]; then
